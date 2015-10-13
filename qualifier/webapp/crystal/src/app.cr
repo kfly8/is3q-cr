f01866c3971c3bd072fcffb5be460d777759fb26
Kenta, Kobayashi <kentafly88@gmail.com>
2015-10-13 19:20:01 +0900

    init amethyst

--- qualifier/webapp/crystal/src/app.cr
+++ qualifier/webapp/crystal/src/app.cr
@@ -1,5 +1,24 @@
-require "./app/*"
+require "amethyst"
 
-module App
-  # TODO Put your code here
+class WorldController < Base::Controller
+  actions :hello
+
+  view "hello", "#{__DIR__}/../views"
+  def hello
+    @name = "World"
+    respond_to do |format|
+      format.html { render "hello" }
+    end
+  end
 end
+
+class HelloWorldApp < Base::App
+  routes.draw do
+    all "/",      "world#hello"
+    get "/hello", "world#hello"
+    register WorldController
+  end
+end
+
+app = HelloWorldApp.new
+app.serve
