ad3bf695518bf992e8eeb8e4f59a505fbf5cb90d
Kenta, Kobayashi <kentafly88@gmail.com>
2015-10-16 14:52:17 +0900

    Added mysql connection

--- qualifier/webapp/crystal/app.cr
+++ qualifier/webapp/crystal/app.cr
@@ -1,4 +1,6 @@
 require "amethyst"
+require "mysql"
+require "json"
 
 class Config
   json_mapping({
@@ -16,9 +18,34 @@ class DatabaseConfig
   })
 end
 
+class Isucon3Controller < Base::Controller
+  actions :index
+
+  def setting
+    path   = File.dirname(__FILE__) + "/../config/#{ ENV["ISUCON_ENV"]? || "local" }.json"
+    ret    = File.read(path)
+    return Config.from_json(ret)
+  end
+
+  def connection
+    conf = setting
+
+    MySQL.connect(
+      conf.database.host,
+      conf.database.username,
+      conf.database.password,
+      conf.database.dbname,
+      conf.database.port,
+      nil
+    )
+  end
 
   view "hello", "#{__DIR__}/views"
   def index
+    mysql = connection
+    ret = mysql.query("SELECT RAND()")
+
+    @name = ret
     respond_to do |format|
       format.html { render "hello" }
     end
